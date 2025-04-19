e8c88739256e3d952111cf1033be862efa5b8b35
Russ <wajidjalal3@gmail.com>
2025-04-19 17:24:18 +0300

    Add files via upload
    TOOL CRATE COMBO KURDISH

---
+++ combo.aslly.py
@@ -0,0 +1,137 @@
+import random,os,time,sys
+
+from names import get_first_name
+os.system("pkg install espeak")
+#_____CLOUOR_____#
+BLACK = "\033[0;30m"
+RED = "\033[0;31m"
+GREEN = "\033[0;32m"
+BROWN = "\033[0;33m"
+BLUE = "\033[0;34m"
+PURPLE = "\033[0;35m"
+CYAN = "\033[0;36m"
+LIGHT_GRAY = "\033[0;37m"
+DARK_GRAY = "\033[1;30m"
+LIGHT_RED = "\033[1;31m"
+LIGHT_GREEN = "\033[1;32m"
+YELLOW = "\033[1;33m"
+LIGHT_BLUE = "\033[1;34m"
+LIGHT_PURPLE = "\033[1;35m"
+LIGHT_CYAN = "\033[1;36m"
+LIGHT_WHITE = "\033[1;37m"
+BOLD = "\033[1m"
+FAINT = "\033[2m"
+ITALIC = "\033[3m"
+UNDERLINE = "\033[4m"
+BLINK = "\033[5m"
+NEGATIVE = "\033[7m"
+CROSSED = "\033[9m"
+END = "\033[0m"
+
+os.system('clear')
+
+name= ["Zizo", "aram", "nawzad", "sarkawt", "ahmad", "hama", "sardar", "karox","kurd", "rebaz", "zana", "baran", "rezhin", "rozh", "layla", "barzani", "darin", "aso", "kawa", "narmin", "daro", "anas", "itzaha", "qqqqq", "sardam", "zhilan", "laro","hunar",'sara',"lawand","mahmad",'mahammad','muhamad','muhammad','hamoka','ali','abdulla','abdula','halwest','nariman','barzan','arin','fenk','barin','hussen','husen','hasan','hassan','tahseen','tahsen','tahsin','kurdistan','kurdstan','omar','sirwan','eman','avan','lava','lania','lona','bts','btskurd','kurdbts','blackpink','blackpinkkurd','kurdblackpink','shaima','shaema','rawa','shad','zhila','zhilia','zhyar','zhiar','zheer','zhir','zher','zhear','redar','soz','faraydun','faraidun','kare','kale','brusk','brwsk','payam','osman','husman','hwsman','shno','shahen','paiman','payman','ramazan','qadr','abdullqadr','abdulqadr','nawroz','nauroz','xoshnaw','blbas','bilbas','saman','rezhwan','rezhuan','avin','ibrahem','ibrahim','ibrahym','aland','shaean','shaian','najat','daniar','danyar','danear','dalia','akar','akam','dalea','dahen','dahin','rezan','rezha','hersh','parwin','parwen','parwyn','jihan','aran','azhin','aihan','alan','adam','raman','blnd','balen','shazada','pasha','ismahil','sma','ismahel','malik','aha','a7a','7ama','soran','lawa','lauand','hogr','baian','baean','rzgar','yusf','ziad','zyad','zead','rayan','hemn','zhyan','zhean','zhian','mahnaz','ayub','aiub','yaxi','amanj','bedar','sardar','zrar','alin','ailan','rewan','rahand','rawand','kako','sangar','salar','salm','salam','sleman','zryan','zrian','xabat','zina','aya','asma','ako','runaz','rwnaz','abo','7abo','3abo','saya','astera','karim', 'azad', 'ramo', 'salah', 'salh', 'zaka', 'zakarya', 'zakaria', 'mariwan', 'marewan', 'bahez', 'bahiz', 'sakar', 'pubg', 'baby', 'baban', 'alnd', 'baxtyar', 'baxtiar', 'rebwar', 'rebuar', 'nazdar', 'ayman', 'aiman', 'aeman']
+
+raqam= ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "12", "123", "1234", "12345", "123456", "1234567", "12345678", "123456789", "1234567890", "1212", "2112", "2121", "1122", "11", "112233", "11223344", "1122334455", "112233445566", "123321", "12344321", "1234554321", "123456654321", "1010", "2020", "3030", "4040", "5050", "6060", "7070", "8080", "9090", "1100", "2200", "3300", "4400", "5500", "6600", "7700", "8800", "9900", "0011", "0022", "0033", "0044", "0055", "0066", "0077", "0088", "0099", ".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", ".0", ".12", ".123", ".1234", ".12345", ".123456", ".1234567", ".12345678", ".123456789", ".1234567890", ".11", ".1122", ".112233", ".11223344", ".1122334455", ".1212", ".2121", ".123321", ".1010", ".2121", ".9999", ".1111", ".8282", ".12123232","2000","2010","2009","2008","2007","2006","2005","2004","2003","2002","2001"]
+#_____espeak_____
+os.system("espeak plase")
+os.system("espeak join")
+os.system("espeak my")
+os.system("espeak chanall")
+
+print(f"{GREEN} [+] PLASE JOIN MY CHANNAL 😌🙏")
+time.sleep(3)
+os.system("xdg-open https://t.me/RUSS10")
+time.sleep(1)
+os.system("clear")
+#_____run____#
+def run(e):
+  for t in e:
+    sys.stdout.write(t),sys.stdout.flush()
+    time.sleep(0.01)
+#___________banner______#
+def banner():
+  logo=(f"""
+  {YELLOW}
+
+ ██████╗ ██████╗ ███╗   ███╗██████╗  ██████╗ 
+██╔════╝██╔═══██╗████╗ ████║██╔══██╗██╔═══██╗
+██║     ██║   ██║██╔████╔██║██████╔╝██║   ██║
+██║     ██║   ██║██║╚██╔╝██║██╔══██╗██║   ██║
+╚██████╗╚██████╔╝██║ ╚═╝ ██║██████╔╝╚██████╔╝
+ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚═════╝  ╚═════╝ 
+  {BLUE}                                             
+██████╗ ██╗   ██╗███████╗███████╗            
+██╔══██╗██║   ██║██╔════╝██╔════╝            
+██████╔╝██║   ██║███████╗███████╗            
+██╔══██╗██║   ██║╚════██║╚════██║            
+██║  ██║╚██████╔╝███████║███████║            
+╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝  
+IM CODER AND CRACKER 🙂💔
+  """)
+  print(logo)
+  print("")
+  print(f"{YELLOW}_"*80)
+
+#_______menue_______#
+def mainn():
+   banner()
+   print("")
+   print("[1] crate combo kurdish")
+   print("[2] crate combo english")
+   print("[00] exit(menue)")
+   chu=input(f"{BROWN}CHOOSE: ")
+   if chu == "1":
+      kurdish()
+   elif chu == "2":
+      english()
+   elif chu == "00":
+      os.system('clear')
+      mainn()
+   else:
+     os.system('clear')
+     mainn()
+  
+#_______combo kurdish_____#
+def kurdish():
+    os.system("clear")
+    banner()
+    run(f"{LIGHT_WHITE}[@gmail.com | @yahoo.com | @hotmail.com] ")
+    em=input("selcet: ")
+    run(f"{LIGHT_GREEN} [1000 | 2000 | 3000] ")
+    limit=input("selcet: ")
+    sav=input(f"{CYAN}save combo /sdcard/")
+    for _ in range(int(limit)):
+        e=random.choice(name)
+        w=random.choice(raqam)
+        email=str(e)+str(w)+em
+        password=str(e)+str(w)
+        with open('/sdcard/'+sav+'.txt','a')as f:
+            f.write(email+':'+password+'\n')
+    print("")
+    print(f"{GREEN}YES SAVE TOTALL ACCOUNT COMBO")
+    time.sleep(3)
+    exit()
+
+#_______combo english_______#
+def english():
+   os.system("clear")
+   banner()
+   run(f"[@gmail.com | @yahoo.com | @hotmail.com] ")
+   em=input("selcet: ")
+   run(f"{LIGHT_GREEN} [1000 | 2000 | 3000] ")
+   limit=input("selcet: ")
+   sav=input(f"{CYAN}save combo /sdcard/")             
+   for _ in range(int(limit)):
+        e=get_first_name()
+        w=random.choice(raqam)
+        email=str(e)+str(w)+em
+        password=str(e)+str(w)
+        with open('/sdcard/'+sav+'.txt','a')as f:
+            f.write(email+':'+password+'\n')
+   print("")
+   print(f"{GREEN}YES SAVE TOTALL ACCOUNT COMBO")
+   time.sleep(3)
+   exit()   
+  
+mainn()                                          
